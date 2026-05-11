ccd2e31c51023726db9126b6b39f79f76d0e6a4b
lucasbsantana2007 <lucassantana220903@gmail.com>
2026-05-11 17:00:45 -0300

    campo e fundo de inicio

--- main.py
+++ main.py
@@ -28,6 +28,19 @@ FONTE_MEDIA = pygame.font.SysFont(None, 50)
 FONTE_PEQUENA = pygame.font.SysFont(None, 32)
 FONTE_GOL = pygame.font.SysFont(None, 130)
 
+campo = pygame.image.load("assets/campo.png").convert()
+campo = pygame.transform.scale(campo, (LARGURA, ALTURA))
 
+fundo_inicio = pygame.image.load("assets/Fundoprimeiratela.png").convert()
+fundo_inicio = pygame.transform.scale(fundo_inicio, (LARGURA, ALTURA))
+
+bola_img = pygame.image.load("assets/bolanova.png").convert_alpha()
+bola_img = pygame.transform.scale(bola_img, (70, 47))
+
+goleiros = [
+    pygame.transform.scale(pygame.image.load("assets/GoleiroDuda.png").convert_alpha(), (140, 140)),
+    pygame.transform.scale(pygame.image.load("assets/GoleiroLucas.png").convert_alpha(), (140, 140)),
+    pygame.transform.scale(pygame.image.load("assets/GoleiroDudu.png").convert_alpha(), (140, 140)),
+]
 
 
