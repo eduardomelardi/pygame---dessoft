f0159496348a801739376f78fa0acc699e2ec2f1
lucasbsantana2007 <lucassantana220903@gmail.com>
2026-05-11 16:41:03 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print('oi')
+import pygame
+import sys
+import random
+import math
 
-print ("testando")
+pygame.init()
+
+
+LARGURA = 900
+ALTURA = 600
+FPS = 60
+
+
+TELA = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Jogo de Penalti")
+RELOGIO = pygame.time.Clock()
+
